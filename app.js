d721bd69321ada14b376f75b9413b51ecb52171a
E-M-Sz <emsz.dev@gmail.com>
2017-10-08 23:07:01 +0200

    Added js folder, cleaned app.js code

--- app.js
+++ app.js
@@ -11,17 +11,15 @@ app.use(express.static('public'));
 var io = require('socket.io')(server);
 
 io.sockets.on('connection', function (socket) {
-    console.log("New client: " + socket.id);
+  console.log("New client: " + socket.id);
 
-    socket.on('mouse', function(data) {
-        console.log("Received:" + data);
+  socket.on('mouse', function(data) {
+    console.log("Received:" + data);
 
-        socket.broadcast.emit('mouse', data);
-      }
-    );
+    socket.broadcast.emit('mouse', data);
+  });
 
-    socket.on('disconnect', function() {
-      console.log("Client has disconnected");
-    });
-  }
-);
+  socket.on('disconnect', function() {
+    console.log("Client has disconnected");
+  });
+});
